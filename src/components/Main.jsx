398b63e1b6980e7102a9d4df989fdd9767c57379
Mirko <mirkocaccavone03@gmail.com>
2025-02-12 18:17:00 +0100

    [feat] Add Header, Main, and Footer components

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (
+        <main>
+            <div className="container">
+                <div className="content-main">
+                    {">"} Content goes here {"<"}
+                </div>
+            </div>
+        </main>
+    )
+}
