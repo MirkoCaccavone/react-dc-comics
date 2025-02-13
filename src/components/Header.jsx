2c3ff8c6b0bdeb52990c2d17e28516a45a471069
Mirko <mirkocaccavone03@gmail.com>
2025-02-13 16:22:24 +0100

    [feat] add navigation links to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
 import HeaderNav from "./HeaderNav";
 
+const linksHeader = [
+    { id: 1, text: "CHARACTERS", url: "#", current: false },
+    { id: 2, text: "COMICS", url: "#", current: true },
+    { id: 3, text: "MOVIES", url: "#", current: false },
+    { id: 4, text: "TV", url: "#", current: false },
+    { id: 5, text: "GAMES", url: "#", current: false },
+    { id: 6, text: "COLLECTIBLES", url: "#", current: false },
+    { id: 7, text: "VIDEOS", url: "#", current: false },
+    { id: 8, text: "FANS", url: "#", current: false },
+    { id: 9, text: "NEWS", url: "#", current: false },
+    { id: 10, text: "SHOP", url: "#", current: false }
+];
+
 // Header.jsx
+
 export default function Header() {
     return (
         <header>
@@ -9,7 +23,7 @@ export default function Header() {
                     <img src="./img/dc-logo.png" alt="Logo" />
                 </figure>
 
-                <HeaderNav />
+                <HeaderNav links={linksHeader} />
 
             </div>
         </header>
