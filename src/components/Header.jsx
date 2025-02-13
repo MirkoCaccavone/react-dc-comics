f53d65cad76942005b6bca3f622af02c54150418
Mirko <mirkocaccavone03@gmail.com>
2025-02-13 16:05:14 +0100

    [feat] create HeaderNav.jsx, import HeaderNav in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import HeaderNav from "./HeaderNav";
+
 // Header.jsx
 export default function Header() {
     return (
@@ -6,22 +8,9 @@ export default function Header() {
                 <figure>
                     <img src="./img/dc-logo.png" alt="Logo" />
                 </figure>
-                <nav>
-                    <ul>
-                        <li><a href="#">CHARACTERS</a></li>
-                        <li><a href="#" className="active">COMICS</a></li>
-                        <li><a href="#">MOVIES</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">GAMES</a></li>
-                        <li><a href="#">COLLECTIBLES</a></li>
-                        <li><a href="#">VIDEOS</a></li>
-                        <li><a href="#">FANS</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">SHOP</a></li>
 
+                <HeaderNav />
 
-                    </ul>
-                </nav>
             </div>
         </header>
     );
