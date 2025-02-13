3ef1a05e7cb612ecec7178a4e71e5279a772e3dd
Mirko <mirkocaccavone03@gmail.com>
2025-02-13 16:09:42 +0100

    [feat] create Banner.jsx , import banner in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,17 @@
+import Banner from "./Banner"
+
 export default function Main() {
     return (
         <main>
-            <div className="container">
-                <div className="content-main">
-                    {">"} Content goes here {"<"}
+            <div className="up-container-main">
+                <div className="container">
+                    <div className="content-main">
+                        {">"} Content goes here {"<"}
+                    </div>
                 </div>
             </div>
+
+            <Banner />
         </main>
     )
 }
