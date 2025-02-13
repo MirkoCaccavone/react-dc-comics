932784ae00e9e6fc5ebbd776838d6021bb901ac7
Mirko <mirkocaccavone03@gmail.com>
2025-02-13 16:24:03 +0100

    [feat] use props in HeaderNav

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,24 +1,13 @@
-const linksHeader = [
-    { id: 1, text: "CHARACTERS", url: "#", current: false },
-    { id: 2, text: "COMICS", url: "#", current: true },
-    { id: 3, text: "MOVIES", url: "#", current: false },
-    { id: 4, text: "TV", url: "#", current: false },
-    { id: 5, text: "GAMES", url: "#", current: false },
-    { id: 6, text: "COLLECTIBLES", url: "#", current: false },
-    { id: 7, text: "VIDEOS", url: "#", current: false },
-    { id: 8, text: "FANS", url: "#", current: false },
-    { id: 9, text: "NEWS", url: "#", current: false },
-    { id: 10, text: "SHOP", url: "#", current: false }
-];
+
 
 // esportiamo la funzione
-export default function HeaderNav() {
+export default function HeaderNav(props) {
     return (
         <>
             {/* cicliamo con map creando un altro array */}
             <nav>
                 <ul>
-                    {linksHeader.map((link) =>
+                    {props.links.map((link) =>
                         <li><a href={link.url}><span>{link.text}</span></a></li>
                     )}
                 </ul>
